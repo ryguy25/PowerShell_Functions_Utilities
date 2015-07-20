da4261ffa75c294e1fa3a73ae0e94a3e911b26cc
Ryan Brown <ryan.andrew.brown@gmail.com>
2015-07-20 12:40:58 -0700

    Update Invoke-MoveCursorToLocation.ps1
    Re-worked the calculations to use trig functions to determine the movement increments.

--- Invoke-MoveCursorToLocation.ps1
+++ Invoke-MoveCursorToLocation.ps1
@@ -30,7 +30,6 @@ This parameter controls the amount of wait time between moves.  It can be adjust
 .NOTES
 #>
 
-
 [CmdletBinding()]
 
 PARAM (
@@ -45,7 +44,7 @@ PARAM (
     [uint32]$NumberOfMoves = 50,
 
     [Parameter()]
-    [uint32]$CursorDelay = 5
+    [uint32]$WaitBetweenMoves = 50
 
 )
 
@@ -53,21 +52,116 @@ PARAM (
 
         $currentCursorPosition = [System.Windows.Forms.Cursor]::Position
 
-        $xDeltaMovement = ($X - $currentCursorPosition.X) / $NumberOfMoves
-        $yDeltaMovement = ($Y - $currentCursorPosition.Y) / $NumberOfMoves
+        #region - Calculate positiveXChange
+        if ( ( $currentCursorPosition.X - $X ) -ge 0 ) {
+            
+            $positiveXChange = $false
+        
+        }
+        
+        else {
+        
+            $positiveXChange = $true
+        
+        }
+        #endregion - Calculate positiveXChange
 
-        for ( $i = 0 ; $i -lt $NumberOfMoves ; $i++ ) {
+        #region - Calculate positiveYChange
+        if ( ( $currentCursorPosition.Y - $Y ) -ge 0 ) {
+        
+            $positiveYChange = $false
+        
+        }
+        
+        else {
+        
+            $positiveYChange = $true
+        
+        }
+        #endregion - Calculate positiveYChange
 
-            $currentCursorPosition.X += $xDeltaMovement
-            $currentCursorPosition.Y += $yDeltaMovement
+        #region - Setup Trig Values
+
+        ### We're always going to use Tan and ArcTan to calculate the movement increments because we know the x/y values which are always
+        ### going to be the adjacent and opposite values of the triangle
+        $xTotalDelta = [Math]::Abs( $X - $currentCursorPosition.X )
+        $yTotalDelta = [Math]::Abs( $Y - $currentCursorPosition.Y )
+        
+        ### To avoid any strange behavior, we're always going to calculate our movement values using the larger delta value
+        if ( $xTotalDelta -ge $yTotalDelta ) {
+
+            $tanAngle = [Math]::Tan( $yTotalDelta / $xTotalDelta )
+
+            if ( $NumberOfMoves -gt $xTotalDelta ) {
+            
+                $NumberOfMoves = $xTotalDelta
+            
+            }
+
+            $xMoveIncrement = $xTotalDelta / $NumberOfMoves
+            $yMoveIncrement = $yTotalDelta - ( ( [Math]::Atan($tanAngle) * ( $xTotalDelta - $xMoveIncrement ) ) )
+
+        }
+        else {
+
+            $tanAngle = [Math]::Tan( $xTotalDelta / $yTotalDelta )
+
+            if ( $NumberOfMoves -gt $yTotalDelta ) {
+
+                $NumberOfMoves = $yTotalDelta
+
+            }
+
+            $yMoveIncrement = $yTotalDelta / $NumberOfMoves
+            $xMoveIncrement = $xTotalDelta - ( ( [Math]::Atan($tanAngle) * ( $yTotalDelta - $yMoveIncrement ) ) )
+        }
+        #endregion - Setup Trig Values
+
+        #region Verbose Output (Before for loop)
+        Write-Verbose "StartingX: $($currentCursorPosition.X)`t`t`t`t`t`tStartingY: $($currentCursorPosition.Y)"
+        Write-Verbose "Total X Delta: $xTotalDelta`t`t`t`t`tTotal Y Delta: $yTotalDelta"
+        Write-Verbose "Positive X Change: $positiveXChange`t`t`tPositive Y Change: $positiveYChange"
+        Write-Verbose "X Move Increment: $xMoveIncrement`t`t`tY Move Increment: $yMoveIncrement"
+        #endregion
+
+        for ( $i = 0 ; $i -lt $NumberOfMoves ; $i++) {
+            
+            ##$yPos = [Math]::Atan($tanAngle) * ($xTotalDelta - $currentCursorPosition.X)
+            ##$yMoveIncrement = $yTotalDelta - $yPos
+
+            #region Calculate X movement direction
+            switch ( $positiveXChange ) {
+
+                $true    { $currentCursorPosition.X += $xMoveIncrement }
+                $false   { $currentCursorPosition.X -= $xMoveIncrement }
+                default  { $currentCursorPosition.X = $currentCursorPosition.X }
+
+            }
+            #endregion Calculate X movement direction
+
+            #region Calculate Y movement direction
+            switch ( $positiveYChange ) {
+
+                $true    { $currentCursorPosition.Y += $yMoveIncrement }
+                $false   { $currentCursorPosition.Y -= $yMoveIncrement }
+                default  { $currentCursorPosition.Y = $currentCursorPosition.Y }
+
+            }
+            #endregion Calculate Y movement direction
 
             [System.Windows.Forms.Cursor]::Position = $currentCursorPosition
+            Start-Sleep -Milliseconds $WaitBetweenMoves
 
-            Start-Sleep -Milliseconds $CursorDelay
+            #region Verbose Output (During Loop)
+            Write-Verbose "Current X Position:`t $($currentCursorPosition.X)`tCurrent Y Position: $($currentCursorPosition.Y)"
+            #endregion Verbose Output (During Loop)
+        }
         
-        }        
-        
-
+        $currentCursorPosition.X = $X
+        $currentCursorPosition.Y = $Y
+        [System.Windows.Forms.Cursor]::Position = $currentCursorPosition
+        Write-Verbose "End X Position: $($currentCursorPosition.X)`tEnd Y Position: $($currentCursorPosition.Y)"
+    
     }
 
     Catch {
